adbc0f1e454a0eebb63d51590be9ab5059e0ecbd
Lukmajcher <100759817+LMajcherWsei@users.noreply.github.com>
2022-03-15 20:21:54 +0100

    Trzecie zasilenie zadania do 10

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,12 +1,15 @@
 package pl.lublin.wsei.java.cwiczenia;
+
+import java.util.Scanner;
+
 /**
- *   Komentarz javadoc
- *   @author Lukasz Majcher
+ * Komentarz javadoc
  *
+ * @author Lukasz Majcher
  */
 
-public class  Main {
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         System.out.print("Ala");
         System.out.print("ma");
         System.out.print("kota\n");
@@ -23,18 +26,28 @@ public class  Main {
         double b = 4.21;
         String s = "jakiś tekst";
 
-        System.out.printf("a = %d, b = %.2f, s = %20s %n", a,b,s);
+        System.out.printf("a = %d, b = %.2f, s = %20s %n", a, b, s);
 
         System.out.printf("Nazywaliśmy to \"witaminą B3\"\n");
 
         System.out.printf("alfa\tsin(alfa)\n");
-        for (int i = 0; i < 370; i+=10)
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        for (int i = 0; i < 370; i += 10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
 
         // Komentarz jednolinijkowy
         /*
             Komentarz
             wielolinijkowy
          */
+
+        Scanner input = new Scanner(System.in);
+        int num1 = 0, num2 = 0;
+        do {
+            System.out.print("Podaj liczby, które mam dodać: ");
+            num1 = input.nextInt();
+            num2 = input.nextInt();
+            if ((num1 == 0) || (num2 == 0)) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        } while (true);
     }
 }
